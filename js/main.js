57ba85feb20c06cb6ff495b39bf1e27fb6a3ad7f
Matt Vitello <mattvitello@Matts-MacBook-Pro.local>
2017-10-19 17:43:19 -0500

    new site

---
+++ js/main.js
@@ -0,0 +1,10 @@
+    /*
+    $(function(){
+        var x = 0;
+        setInterval(function(){
+            x-=1;
+            $('body').css('background-position', x + 'px 0');
+            $('html').css('background-position', x + 'px 0');
+        }, 30);
+    })
+    */
